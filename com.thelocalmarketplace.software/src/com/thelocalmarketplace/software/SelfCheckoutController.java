ffb4e5c528a2a14345cd550ccba3ff4c10688e6c
Nikki Kim <117608863+nnkim999@users.noreply.github.com>
2023-11-03 19:13:48 -0600

    Added scenarios for WeightDiscrepancyDetected.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutController.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutController.java
@@ -1,3 +1,10 @@
+//Robin Bowering UCID 30123373
+//Matt Gibson UCID 30117091
+//Kelvin Jamila UCID 30117164
+//Nikki Kim UCID 30189188
+//Hillary Nguyen UCID 30161137
+
+
 package com.thelocalmarketplace.software;
 import com.jjjwelectronics.IDevice;
 import com.jjjwelectronics.IDeviceListener;
