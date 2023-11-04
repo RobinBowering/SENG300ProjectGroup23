8a6b3b7f823b4b938b49deca7dfb8fb78a9f7e85
robinbowering <robinbowering@10.0.0.35>
2023-11-03 23:26:43 -0600

    Some test class progress

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestClass.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestClass.java
@@ -1,5 +1,22 @@
 package com.thelocalmarketplace.software.test;
 
-public class TestClass {
+import org.junit.*;
+import com.jjjwelectronics.*;
+import com.thelocalmarketplace.hardware.SelfCheckoutStation;
+import com.tdc.*;
+import powerutility.*;
 
+public class TestClass {
+	
+	SelfCheckoutStation station = new SelfCheckoutStation();
+	PowerGrid grid = PowerGrid.instance();
+
+	@Before public void setup() {
+		station.plugIn(grid);
+		station.turnOn();
+		
+	}
+	
+	
+	
 }
