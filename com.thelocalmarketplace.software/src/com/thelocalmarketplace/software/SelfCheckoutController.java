22aa362e767e89704fdb9432428f771bd505c559
robinbowering <robinbowering@10.0.0.35>
2023-11-04 00:53:19 -0600

    Removed unused imports from SelfCheckoutController

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutController.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutController.java
@@ -2,19 +2,8 @@ package com.thelocalmarketplace.software;
 
 import java.math.BigDecimal;
 
-import com.jjjwelectronics.IDevice;
-import com.jjjwelectronics.IDeviceListener;
-import com.jjjwelectronics.Mass;
-import com.jjjwelectronics.scale.ElectronicScaleListener;
-import com.jjjwelectronics.scale.IElectronicScale;
-import com.jjjwelectronics.scanner.Barcode;
-import com.jjjwelectronics.scanner.BarcodeScannerListener;
-import com.jjjwelectronics.scanner.IBarcodeScanner;
-import com.tdc.IComponent;
-import com.tdc.IComponentObserver;
-import com.tdc.coin.*;
+
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
-import com.thelocalmarketplace.hardware.external.ProductDatabases;
 
 public class SelfCheckoutController {
 	
@@ -32,21 +21,22 @@ public class SelfCheckoutController {
 	 */
 	public void startSession() {
 		
-		if (allEnabled()) {
+		
 			
-			SelfCheckoutSession session = new SelfCheckoutSession(hardware,this);
+		SelfCheckoutSession session = new SelfCheckoutSession(hardware,this);
 			
-			hardware.baggingArea.register(session);
-			hardware.scanner.register(session);
-			hardware.coinSlot.attach(session);
-			hardware.coinValidator.attach(session);
-			hardware.coinStorage.attach(session);
-		}
+		hardware.baggingArea.register(session);
+		hardware.scanner.register(session);
+		hardware.coinSlot.attach(session);
+		hardware.coinValidator.attach(session);
+		hardware.coinStorage.attach(session);
 		
 	}
 	private boolean allEnabled() {
 		if ( !hardware.baggingArea.isDisabled() ) {
-			if ( !hardware.baggingArea)
+			if ( !hardware.scanner.isDisabled() {
+				
+			}
 		}
 			
 	}
