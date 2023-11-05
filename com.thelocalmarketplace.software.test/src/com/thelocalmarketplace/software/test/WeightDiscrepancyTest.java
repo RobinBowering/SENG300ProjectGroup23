dcd367ec212360eaff19ef72896ee85f7bddf71e
RobinBowering <124980215+RobinBowering@users.noreply.github.com>
2023-11-04 21:12:15 -0600

    Set Up Product Database and Added a Few Test Cases

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/WeightDiscrepancyTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/WeightDiscrepancyTest.java
@@ -10,12 +10,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jjjwelectronics.Item;
+import com.jjjwelectronics.Mass;
+import com.jjjwelectronics.Numeral;
 import com.jjjwelectronics.scale.ElectronicScale;
+import com.jjjwelectronics.scanner.BarcodedItem;
+import com.jjjwelectronics.scanner.Barcode;
 import com.tdc.CashOverloadException;
 import com.tdc.DisabledException;
 import com.tdc.coin.Coin;
 import com.tdc.coin.CoinSlot;
+import com.thelocalmarketplace.hardware.BarcodedProduct;
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
+import com.thelocalmarketplace.hardware.external.ProductDatabases;
 import com.thelocalmarketplace.software.SelfCheckoutController;
 import com.thelocalmarketplace.software.SelfCheckoutSession;
 
@@ -23,20 +30,44 @@ import powerutility.PowerGrid;
 
 public class WeightDiscrepancyTest {
 	
+
+	Numeral[] testNumeralArray1 = {Numeral.one} ;
+	Barcode testBarcode1 = new Barcode(testNumeralArray1);
+	BarcodedItem testItem1 = new BarcodedItem(testBarcode1, Mass.ONE_GRAM);
+	BarcodedProduct testProduct1 = new BarcodedProduct(testBarcode1, "1 ml of Water", 1, Mass.ONE_GRAM.inGrams().doubleValue());
 	
+	Numeral[] testNumeralArray2 = {Numeral.two} ;
+	Barcode testBarcode2 = new Barcode(testNumeralArray2);
+	BarcodedItem testItem2 = new BarcodedItem(testBarcode2, Mass.ONE_GRAM.sum(Mass.ONE_GRAM));
+	BarcodedProduct testProduct2 = new BarcodedProduct(testBarcode2, "1 ml of Milk", 2, Mass.ONE_GRAM.sum(Mass.ONE_GRAM).inGrams().
+			doubleValue());
+	
+		
+	
+	SelfCheckoutStation hardware;
 	SelfCheckoutSession session;
 	Coin coin;
+	
+
+	
+	
+	
 	
 	
 	@Before 
 	public void setup() {
-		SelfCheckoutStation hardware = new SelfCheckoutStation();
+		hardware = new SelfCheckoutStation();
         hardware.plugIn(PowerGrid.instance());
         hardware.turnOn();
         
         SelfCheckoutController controller = new SelfCheckoutController(hardware);
         
 		SelfCheckoutSession session = new SelfCheckoutSession(hardware, controller);
+		
+		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(testBarcode1, testProduct1);
+		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(testBarcode2, testProduct2);
+		
+
 	}
 	
 	@Test(expected = DisabledException.class)
@@ -51,6 +82,28 @@ public class WeightDiscrepancyTest {
 		session.payingForOrder = false;
 		session.weightDiscrepancyDetected();
 		assertEquals(true, session.scanner.isDisabled());
+	}
+	
+	@Test
+	public void triggerDiscrepancyUnexpectedItem() {
+		hardware.baggingArea.addAnItem(null);
+		session.theMassOnTheScaleHasChanged(hardware.baggingArea, Mass.ONE_GRAM);
+		assertTrue(session.weightDiscrepancy);
+	}
+	@Test
+	public void resolveDiscrepancyUnexpectedItem() {
+		session.theMassOnTheScaleHasChanged(hardware.baggingArea, Mass.ONE_GRAM);
+		session.theMassOnTheScaleHasChanged(hardware.baggingArea, Mass.ZERO);
+		assertFalse(session.weightDiscrepancy);
+	}
+	@Test
+	public void triggerDiscrepancyItemRemoved() {
+		
+	}
+	
+	@After
+	public void clearDatabase() {
+		ProductDatabases.BARCODED_PRODUCT_DATABASE.clear();
 	}
 	
 }
