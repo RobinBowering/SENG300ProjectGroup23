e881df84ad7d6d6038aaaf8a206aebb5460c16df
Nikki Kim <117608863+nnkim999@users.noreply.github.com>
2023-11-04 21:19:43 -0600

    Update AddItemTest.java
    dsewdwdfdgfdfs

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddItemTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddItemTest.java
@@ -13,8 +13,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jjjwelectronics.Mass;
+import com.jjjwelectronics.Numeral;
 import com.jjjwelectronics.scale.ElectronicScale;
 import com.jjjwelectronics.scanner.Barcode;
+import com.jjjwelectronics.scanner.BarcodedItem;
 import com.tdc.CashOverloadException;
 import com.tdc.DisabledException;
 import com.tdc.coin.Coin;
@@ -28,20 +31,30 @@ import ca.ucalgary.seng300.simulation.NullPointerSimulationException;
 import powerutility.PowerGrid;
 
 public class AddItemTest {
+	
+	Numeral[] testNumeralArray1 = {Numeral.one} ;
+	Barcode testBarcode1 = new Barcode(testNumeralArray1);
+	BarcodedItem testItem1 = new BarcodedItem(testBarcode1, Mass.ONE_GRAM);
+	BarcodedProduct testProduct1 = new BarcodedProduct(testBarcode1, "1 ml of Water", 1, Mass.ONE_GRAM.inGrams().doubleValue());
+	
+	Numeral[] testNumeralArray2 = {Numeral.two} ;
+	Barcode testBarcode2 = new Barcode(testNumeralArray2);
+	BarcodedItem testItem2 = new BarcodedItem(testBarcode2, Mass.ONE_GRAM.sum(Mass.ONE_GRAM));
+	BarcodedProduct testProduct2 = new BarcodedProduct(testBarcode2, "1 ml of Milk", 2, Mass.ONE_GRAM.sum(Mass.ONE_GRAM).inGrams().
+			doubleValue());
+	
+		
 
 	SelfCheckoutSession session;
 	Coin coin;
 	Barcode barcode;
 
-	
-	
+		
 	@Before 
 	public void setup() {
 		SelfCheckoutStation hardware = new SelfCheckoutStation();
         hardware.plugIn(PowerGrid.instance());
         hardware.turnOn();
-        
-     
         
         SelfCheckoutController controller = new SelfCheckoutController(hardware);
         
@@ -50,9 +63,10 @@ public class AddItemTest {
 	
 	
 	//test for barcode scanner enabled, but invalid barcode (null)
-	@Test(expected = NullPointerException.class)
+	@Test
 	public void NullBarcode() {
-		session.addItem(null);
+		session.addItem(testBarcode1);
+		assertEquals(1, session.orderTotal);
 	}
 	
 	
@@ -61,7 +75,6 @@ public class AddItemTest {
 	@Test(expected = NullPointerException.class)
 	public void ValidBarcode() {
 		session.addItem(barcode);
-		
 	}
 	
 	
@@ -78,4 +91,19 @@ public class AddItemTest {
 		session.payWithCoin();
 		assertTrue(session.scanner.isDisabled());
 	}
+	
+	/**
+	//test for discrepancy check overload
+	public void DiscrepancyCheckNoOverload() {
+		session.expectedMassOnScale = BigDecimal.valueOf(100);
+		
+		session.actualMassOnScale = BigDecimal.valueOf(150);
+	      
+	
+	        // Call the discrepancyCheck method
+	        session.discrepancyCheck();
+	
+	        // Assert that weightDiscrepancy is not set
+	        assertFalse(session.isWeightDiscrepancy());
+	    }**/
 }
